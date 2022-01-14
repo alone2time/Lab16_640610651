858400a1f639a10ad174c4fc90e7b85a0503ffce
TonPKG <piyaphat_kh@cmu.ac.th>
2022-01-14 17:23:54 +0700

    lab16_1.cpp Completed

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,22 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-
-	
-	
+	int a=5;
+	char b='A';	
+	char &c=b;
+	void *x=&a;
+	void *y=&b;
+	void *z=x;
+	cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<endl;
+	cout<<&a<<" "<<(void *)&b<<" "<<(void *)&c<<" "<<&x<<" "<<&y<<" "<<&z<<endl;
+	c='F';
+	cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<endl;
+	c='W';
+	cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<endl;
+	*(int *)x=6;
+	cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<endl;
+	*(int *)z=7;
+	cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<endl;
 	return 0;
 }
